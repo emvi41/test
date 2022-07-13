7c4cd3f1e6829789c2607cf1fbbdfe6623d247d8
emvi41 <65525869+emvi41@users.noreply.github.com>
2022-07-13 21:13:37 +0900

    test

---
+++ index.js
@@ -0,0 +1,33 @@
+// 탭 버튼 스크립트
+function openCity(cityName, elmnt, color) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].style.backgroundColor = "";
+  }
+
+  // Show the specific tab content
+  document.getElementById(cityName).style.display = "block";
+  elmnt.style.backgroundColor = color;
+}
+
+document.getElementById("defaultOpen").click();
+
+//전화 걸기 스크립트
+function phoneCall(phoneNumber) {
+    location.href = "tel:" + 01045869620;
+  }
+
+// 이미지 슬라이드 스크립트
+function myFunction(imgs) {
+  var expandImg = document.getElementById("expandedImg");
+  var imgText = document.getElementById("imgtext");
+  expandImg.src = imgs.src;
+  imgText.innerHTML = imgs.alt;
+  expandImg.parentElement.style.display = "block";
+}
